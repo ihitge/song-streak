306be7d3cb76de6da1e45c5bbb7d93b04030fd90
Ian H <ian@benchmarkme.com>
2025-12-03 14:36:50 +0000

    refactor: update theme color access to use separate LightThemeColors and DarkThemeColors objects instead of a single Colors object.

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -5,7 +5,7 @@
 
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
-import { Colors } from '@/constants/Colors';
+import { DarkThemeColors, LightThemeColors } from '@/constants/Colors';
 import { useColorScheme } from './useColorScheme';
 
 type ThemeProps = {
@@ -18,7 +18,7 @@ export type ViewProps = ThemeProps & DefaultView['props'];
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+  colorName: keyof typeof LightThemeColors & keyof typeof DarkThemeColors
 ) {
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
@@ -26,7 +26,7 @@ export function useThemeColor(
   if (colorFromProps) {
     return colorFromProps;
   } else {
-    return Colors[theme][colorName];
+    return theme === 'light' ? LightThemeColors[colorName] : DarkThemeColors[colorName];
   }
 }
 
